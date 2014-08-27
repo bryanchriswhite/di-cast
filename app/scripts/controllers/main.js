5f485308a76153404b3f94b0137ba663b85a6d95
Bryan White <bryanchriswhite@gmail.com>
2014-08-27 04:58:38 +0200

    loading media on chromecast

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the diCastApp
  */
 angular.module('diCastApp')
-  .controller('MainCtrl', function ($scope, channelService) {
+  .controller('MainCtrl', function ($scope, $timeout, channelService) {
     channelService.list()
       .success(function (channels) {
         $scope.channels = channels;
@@ -16,4 +16,75 @@ angular.module('diCastApp')
       .error(function (reason) {
         console.error('couldn\'t fetch channels: ', reason)
       })
+
+    $scope.channel = {
+      key: null
+    };
+
+    var initializeCastApi = function () {
+      var sessionRequest = new chrome.cast.SessionRequest('2722D3A6');
+      var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+        sessionListener,
+        receiverListener);
+      chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+
+      chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+    };
+
+    (function init() {
+      if (!chrome.cast || !chrome.cast.isAvailable) {
+        $timeout(init, 1000);
+      } else {
+        initializeCastApi();
+      }
+    }())
+
+    function receiverListener(e) {
+      if (e === chrome.cast.ReceiverAvailability.AVAILABLE) {
+      }
+    }
+
+    function sessionListener(e) {
+      console.log('sessoinlistener')
+      console.log(e);
+      $scope.session = e;
+    }
+
+    function onInitSuccess() {
+      console.log('onInitSuccess: ', [].slice.apply(arguments).join(', '))
+    }
+
+    function onRequestSessionSuccess(e) {
+      console.log('resuestsessionsuccess')
+      console.log(e)
+    }
+
+    function onError() {
+      console.log('onError: ', [].slice.apply(arguments).join(', '))
+    }
+
+    function onLaunchError(reason) {
+      console.error('launchError: ', reason)
+    }
+
+    function onMediaDiscovered() {
+      console.log('onMediaDiscovered called!')
+    }
+
+    function onMediaError(reason) {
+      console.error('onMediaError: ', reason)
+    }
+
+    $scope.$watch('channel.key', function (newVal) {
+      if (newVal) {
+        var url = 'http://pub1.di.fm/di_' + newVal
+          , mediaInfo = new chrome.cast.media.MediaInfo(url, 'audio/mpeg')
+          , request = new chrome.cast.media.LoadRequest(mediaInfo)
+          ;
+
+        $scope.session.loadMedia(request,
+          onMediaDiscovered.bind(this, 'loadMedia'),
+          onMediaError);
+      }
+    })
   });
